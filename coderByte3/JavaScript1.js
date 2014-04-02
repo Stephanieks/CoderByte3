adb13df4a90e2747a6e33a7fd34a9c91830fb16c
dannyrodriguez <daniel.rodriguez36@yahoo.com>
2014-04-02 12:01:30 -0500

    Final ABC check commit

---
+++ coderByte3/JavaScript1.js
@@ -0,0 +1,19 @@
+﻿function text() {
+    "use strict";
+    var ab = "ad  b";
+    return ab;
+}
+function aBCheck(str) {
+    var text = str();
+    var arrText = text.split("");
+    var result = false; 
+    for (var i = 0; i < arrText.length; i++) {
+        if (arrText[i] === "a") {
+        } if (arrText[i+3] === "b") {
+            result = true;
+        }
+        document.getElementById("output").innerHTML = result;
+    }
+}
+aBCheck(text);
+
