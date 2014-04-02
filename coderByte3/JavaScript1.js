43a59fd120a7de9cb123bc207e660319991e3d71
dannyrodriguez <daniel.rodriguez36@yahoo.com>
2014-04-02 13:19:38 -0500

    Added Q's

--- coderByte3/JavaScript1.js
+++ coderByte3/JavaScript1.js
@@ -1,6 +1,6 @@
-﻿function text() {
+﻿/*function text() {
     "use strict";
-    var ab = "ad  b";
+    var ab = "axxxb";
     return ab;
 }
 function aBCheck(str) {
@@ -8,12 +8,56 @@ function aBCheck(str) {
     var arrText = text.split("");
     var result = false; 
     for (var i = 0; i < arrText.length; i++) {
-        if (arrText[i] === "a") {
-        } if (arrText[i+3] === "b") {
-            result = true;
+        if (arrText[i] === "a" && arrText[i+4] === "b") {
+           result = true;
         }
         document.getElementById("output").innerHTML = result;
     }
 }
-aBCheck(text);
+aBCheck(text);*/
 
+//Q12 Vowel Count
+/*function sentence() {
+    "use strict";
+    var text = "All cows eat grass";
+    return text;
+}
+
+function vowelCount(str) {
+    "use strict";
+    var text = str().toLowerCase();
+    var lilVowels = ["a","e","i","o","u"];
+    var count = 0;
+    for (var i = 0; i < text.length; i++) {
+        for (var j = 0; j < text.length; j++) {
+            if (text[i] === lilVowels[j]) {
+                count += 1;
+            }
+        }
+    }
+    document.getElementById("output").innerHTML = count;
+}
+vowelCount(sentence);*/
+
+//Q13 Word Count
+/*function sentence() {
+    "use strict";
+    var text = "All cows eat grass";
+    return text;
+}
+function wordCount(str) {
+    "use strict";
+    var text = str();
+    var textArr = text.split(" ");
+    var count = 0;
+    for (var i = 0; i < textArr.length; i++) {
+        count += 1;
+    }
+    document.getElementById("output").innerHTML = count;
+}
+wordCount(sentence);*/
+
+
+
+
+ 
